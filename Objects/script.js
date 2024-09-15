bf9aa70061db528bb4d33c78ad908d369f9a42b4
knking <knr020498@gmail.com>
2024-09-15 12:57:11 +0530

    javascript object and object property like seal & freeze

---
+++ Objects/script.js
@@ -0,0 +1,32 @@
+// let username = 'Anurag'
+
+// const user = {
+//   firstName: 'Adarsh',
+//   lastName: 'Singh',
+//   pata: {
+//     city: 'Bangalore',
+//     pinCode: 876876,
+//     state: 'Karnataka',
+//     moreDetails: {
+//       population: 9798897897,
+//       area: '787 sq km',
+//     },
+//   },
+//   age: 15,
+//   isGraduate: false,
+// }
+
+// // Object.seal(user)
+// Object.freeze(user)
+
+// console.log('isGraduate' in user);
+
+const myUser = {
+    firstname: "Krishna",
+    lastName: "Roy",
+    age:25,
+}
+
+console.log(myUser.age);
+
+let username="nand"

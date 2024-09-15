41e52813fb3d5424f4851ab65a89f05c4a7ee1c8
knking <knr020498@gmail.com>
2024-09-15 15:53:48 +0530

    loops  in js

---
+++ Loops/script.js
@@ -0,0 +1,55 @@
+console.log('Program Started')
+
+// debugger
+
+// let i = 1
+// while(i <= 100) {
+//    console.log(i);
+//    i++
+// }
+
+const friends = ['Akash', 'Rahul', 'Adarsh', 'Arif', 'Gaurav', 'Anurag', 'Rakesh']
+
+let i = 0
+
+while(i < friends.length) {
+    console.log(`${i + 1}. ${friends[i]}`);
+    friends[i] = friends[i] + ' Procodrr'
+    i++
+}
+
+console.log('Program Ended')
+
+console.log('Program Start')
+
+
+for(let i = 0; i <= 10; i++) {
+    if(i % 2 === 0) {
+        console.log(i); 
+    }
+}
+
+console.log('Program Ended')
+
+
+
+console.log('Program Start')
+
+// debugger
+// let i = 0
+// while (i < 5) {
+//   console.log(i)
+//   i++
+// }
+
+// for(let i = 0; i < 5; i++) { 
+//     console.log(i);
+// }
+
+let j = 0 
+do {
+    console.log(i);
+    i++
+} while (j < 5)
+
+console.log('Program Ended')

8e96e151a3a27b4535d08b94df2490b45d5cc1ec
knking <knr020498@gmail.com>
2024-09-15 22:37:48 +0530

    functions in js

---
+++ Functions/script.js
@@ -0,0 +1,27 @@
+
+// Function Definition
+function introduceMe(username, profession, age) {
+    // console.log(typeof username);
+    // console.log(typeof profession);
+    // console.log(typeof age);
+    console.log('Hi,');
+    console.log(`My name is ${username || 'Procodrr'}.`);
+    console.log(`I am a ${profession}`);
+    console.log(`I am ${age} years old.`); 
+}
+
+// //                  Function Call
+// const returnValue = introduceMe()
+
+// introduceMe('Anurag', 'Software Engineer', 25)
+// introduceMe('Akash', 'Mechanical Engineer', 27)
+
+function addTwoNumbers(a, b) {
+    debugger
+    
+        return a + b
+    }
+    
+    // const result = addTwoNumbers(56, 6)
+    
+    // console.log(result);
---
+++ demo.js
@@ -0,0 +1,17 @@
+
+
+const  name1 = "krishna"
+
+function add(){
+    const name1 = "nand"
+    console.log(name1);
+}
+
+function sub(){
+    console.log(name1);
+}
+
+console.log(name1);
+add()
+console.log(name1);
+sub()

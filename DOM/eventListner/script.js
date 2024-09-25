e6c7b5744fe0432653c89edaad00c4ecdb3e7635
knking <knr020498@gmail.com>
2024-09-25 17:26:04 +0530

    event listener in js

--- DOM/eventListner/script.js
+++ DOM/eventListner/script.js
@@ -1,8 +1,43 @@
 
-function sayHi(){
-    console.log("say hi from h1");
+// const h1 = document.querySelector('h1')
+// //send way
+// // h1.onclick= sayHi
+
+// h1.addEventListener('click',sayHi)
+// h1.addEventListener('click', function(){
+//     console.log("hi from fn2");
+    
+// }) 
+
+
+const container = document.querySelector(".container")
+const card = document.querySelector(".card")
+
+// card.addEventListener('click',()=>(
+//     console.log("I am catrd")
+    
+// ))
+let i=1;
+function addCrad(){
+    
+    const newcard = document.createElement('div')
+    newcard.classList.add("card")
+    // const newCrad = card.cloneNode()  // we can ise this also
+    // newcard.classList.remove('add-card')
+    newcard.innerText=`${i++}`
+    container.append(newcard)
+   
     
 }
-function double(){
-    console.log("dble clicked")
-}
+    
+card.addEventListener('click',addCrad)
+
+
+// card.addEventListener('click',()=>{
+//     const newCard= document.createElement('div')
+//     newCard.classList.add('card')
+//     container.append(newCard)
+    
+    
+    
+// })

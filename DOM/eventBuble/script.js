329c36b26595875a36384bea4261ab7e27739258
knking <knr020498@gmail.com>
2024-09-28 10:29:43 +0530

    first css project

---
+++ DOM/eventBuble/script.js
@@ -0,0 +1,17 @@
+
+const green = document.querySelector(".green")
+const pink = document.querySelector(".pink")
+const blue = document.querySelector(".blue")
+
+green.addEventListener("click",(e)=>{
+    console.log("green event listner");
+    
+})
+pink.addEventListener("click",(e)=>{
+    console.log("pink event listner");
+    
+})
+blue.addEventListener("click",(e)=>{
+    console.log("blue event listner");
+    
+})

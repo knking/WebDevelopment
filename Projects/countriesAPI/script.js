e2f6460844609ee15c21537b4ba3ab2ad52a4cb9
knking <knr020498@gmail.com>
2024-10-22 16:54:11 +0530

    project country api filter feature applied

--- Projects/countriesAPI/script.js
+++ Projects/countriesAPI/script.js
@@ -1,5 +1,7 @@
 const countriesContainer = document.querySelector(".countries-container");
 
+const filterByName = document.querySelector(".filter-by-name")
+
 const url = "https://restcountries.com/v3.1/all";
 
 // async function countryData() {
@@ -39,3 +41,36 @@ fetch(url)
   .catch((error) => {
     console.log(error);
   });
+
+  filterByName.addEventListener('change',(e)=>{
+    
+    fetch(`https://restcountries.com/v3.1/region/${filterByName.value}`)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(filterByName.value);
+    countriesContainer.innerHTML = ' '
+    data.forEach((country) => {
+      const countryCard = document.createElement("a");
+      countryCard.classList.add("country-card");
+      countryCard.href=`./country.html?name=${country.name.common}`
+      const cardHTML = ` 
+                <img src="${country.flags.svg}" alt="flag">
+                <div class="card-text">
+                <h3 class="card-title">${country.name.common}</h3>
+                <p><b>Population: </b>${(country.population).toLocaleString('en-IN')}</p>
+                <p><b>Region: </b>${country.region}</p>
+                <p><b>Capital: </b>${country.capital}</p>
+                </div>`;
+                
+      countryCard.innerHTML = cardHTML;
+      countriesContainer.append(countryCard);
+      
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+    
+  })

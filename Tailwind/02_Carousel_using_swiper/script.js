0755dcc99afdf7e41be8172715129514221ae612
knking <knr020498@gmail.com>
2024-11-07 03:41:11 +0530

    second project in tailwind

--- Tailwind/02_Carousel_using_swiper/script.js
+++ Tailwind/02_Carousel_using_swiper/script.js
@@ -3,6 +3,7 @@ var swiper = new Swiper(".mySwiper", {
     grabCursor: true,
     pagination: {
       el: ".swiper-pagination",
+      clickable:true
     },
     navigation: {
       nextEl: ".swiper-button-next",

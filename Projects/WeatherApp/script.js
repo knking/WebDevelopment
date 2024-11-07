0a5e789605a6c56ccdc86c4f7428c992a506d9ed
knking <knr020498@gmail.com>
2024-11-08 03:06:04 +0530

    weather app completed

--- Projects/WeatherApp/script.js
+++ Projects/WeatherApp/script.js
@@ -1,25 +1,49 @@
+const apiKey = "026ffa503460be801ee9a710edd85007";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-const apiKey ="026ffa503460be801ee9a710edd85007"
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const inputBox = document.querySelector(".input-box");
+const btn = document.querySelector(".searchBtn");
+const imageIcon = document.querySelector(".weather-img");
+const weather = document.querySelector(".weather");
+btn.addEventListener("click", () => {
+  checkWeather(inputBox.value);
+  // console.log(inputBox.value)
+});
 
-const inputBox = document.querySelector(".input-box")
-const btn = document.querySelector(".searchBtn")
+async function checkWeather(city) {
+  const responce = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
-btn.addEventListener('click',()=>{
-    checkWeather(inputBox.value) 
-   // console.log(inputBox.value) 
-})
+  if (responce.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    weather.style.display = "none";
+  }
+  else{
+    let data = await responce.json();
 
-
-
-
-async function checkWeather(city){
-    const responce = await fetch(apiUrl+ city + `&appid=${apiKey}`)
-    let data = await responce.json()
-
-    console.log(data)
-    document.querySelector(".city").innerText = data.name
-    document.querySelector(".temp").innerText = Math.round(data.main?.temp) + "°C"
-    document.querySelector(".Humidity").innerText = data.main?.humidity + "%"
-    document.querySelector(".wind").innerText = data.wind.speed + " km/h"
+    weather.style.display = "block";
+  
+    document.querySelector(".city").innerText = data.name;
+    document.querySelector(".temp").innerText =
+      Math.round(data.main?.temp) + "°C";
+    document.querySelector(".Humidity").innerText = data.main?.humidity + "%";
+    document.querySelector(".wind").innerText = data.wind.speed + " km/h";
+  
+    if (data.weather[0].main == "Clear") {
+      imageIcon.src = "./images/clear.png";
+    } else if (data.weather[0].main == "Clouds") {
+      imageIcon.src = "./images/clouds.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      imageIcon.src = "./images/drizzle.png";
+    } else if (data.weather[0].main == "Mist") {
+      imageIcon.src = "./images/mist.png";
+    } else if (data.weather[0].main == "Rain") {
+      imageIcon.src = "./images/Rain.png";
+    } else if (data.weather[0].main == "Snow") {
+      imageIcon.src = "./images/snow.png";
+    } else if (data.weather[0].main == "Wind") {
+      imageIcon.src = "./images/wind.png";
+    }
+  }
+  
 }

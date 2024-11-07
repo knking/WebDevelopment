49a19520fc1e16d5f8d8bd243854e76cdbcca37a
knking <knr020498@gmail.com>
2024-11-07 21:37:03 +0530

    weather app search funcationalty completed

--- Projects/WeatherApp/script.js
+++ Projects/WeatherApp/script.js
@@ -1,10 +1,25 @@
 
 const apiKey ="026ffa503460be801ee9a710edd85007"
-const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=bangalore&units=metric";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
-async function checkWeather(){
-    const responce = await fetch(apiUrl+`appid=${apiKey}`)
+const inputBox = document.querySelector(".input-box")
+const btn = document.querySelector(".searchBtn")
+
+btn.addEventListener('click',()=>{
+    checkWeather(inputBox.value) 
+   // console.log(inputBox.value) 
+})
+
+
+
+
+async function checkWeather(city){
+    const responce = await fetch(apiUrl+ city + `&appid=${apiKey}`)
     let data = await responce.json()
 
     console.log(data)
+    document.querySelector(".city").innerText = data.name
+    document.querySelector(".temp").innerText = Math.round(data.main?.temp) + "°C"
+    document.querySelector(".Humidity").innerText = data.main?.humidity + "%"
+    document.querySelector(".wind").innerText = data.wind.speed + " km/h"
 }

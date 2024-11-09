5b3c69cb51b7bd7a45e5c47bfff4dbb36182a5bd
knking <knr020498@gmail.com>
2024-11-09 06:46:37 +0530

    todo app in js

--- Projects/todoApp/script.js
+++ Projects/todoApp/script.js
@@ -17,11 +17,24 @@ function addTask(){
         i.appendChild(span)
     }
     inputBox.value=""
+    saveData()
 }
 listContainer.addEventListener('click',function(e){
     if(e.target.tagName==="LI"){
         e.target.classList.toggle("checked")
+        saveData()
     }else if( e.target.tagName ==="SPAN"){
          e.target.parentElement.remove()
+         saveData()
     }
 },false)
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML)
+}
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem('data')
+}
+
+showTask()

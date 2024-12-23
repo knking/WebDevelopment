7d44bd138377a66708c42b38e54bdf632925ba72
knking <knr020498@gmail.com>
2024-12-23 13:06:01 +0530

    counter app completed

--- Frontend-mini-challenges/counterApp/script.js
+++ Frontend-mini-challenges/counterApp/script.js
@@ -6,11 +6,32 @@ const decrement =  document.querySelector(".decrement")
 const inputValue = document.querySelector(".inputValue")
 const reset =  document.querySelector(".reset")
 
+let finalAnswer = 0
+let value = 1
 inputValue.addEventListener('input',(e)=>{
-   let value = parseInt(inputValue.value)
-   answer.innerText = value
-   console.log(value);
-   console.log(typeof value);
+value = parseInt(inputValue.value)
    
 })
 
+// function incrementVal(){
+// finalAnswer += value
+// answer.innerText = finalAnswer
+// }
+// function decrementVal(){
+// finalAnswer -=value
+// answer.innerText = finalAnswer
+// }
+
+increment.addEventListener('click',()=>{
+    finalAnswer += value
+answer.innerText = finalAnswer
+})
+decrement.addEventListener('click',()=>{
+    finalAnswer -=value
+answer.innerText = finalAnswer
+})
+
+reset.addEventListener('click',()=>{
+    finalAnswer=0
+    answer.innerText = finalAnswer
+})

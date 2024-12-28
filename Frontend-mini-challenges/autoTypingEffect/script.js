58aa3969bbf99015910fcd7bbb5693840e7dc253
knking <knr020498@gmail.com>
2024-12-28 20:09:16 +0530

    auto typeing completed

--- Frontend-mini-challenges/autoTypingEffect/script.js
+++ Frontend-mini-challenges/autoTypingEffect/script.js
@@ -1,4 +1,5 @@
-const arr = ["Developer", "Designer", "youtuber", "writer"];
+const list1 = ["Developer", "Designer", "youtuber", "writer"];
+const list2 = ["Rider", "writer", "King", "DevOps"];
 // let count = 0
 // let arrCount=0
 // setInterval(()=>{
@@ -30,7 +31,8 @@ const arr = ["Developer", "Designer", "youtuber", "writer"];
 
 // },200)
 
-const span = document.querySelector(".main span");
+const span = document.querySelector("h1 span");
+const span2 = document.querySelector("h2 span")
 
 const word = "Developer";
 let wordIdx = 0;
@@ -38,31 +40,38 @@ let skip = 0;
 let currentIndex = 0;
 let reverseType = false;
 
-const intervalId = setInterval(() => {
-  if (skip) {
-    skip--;
-    return;
-  }
-  if (!reverseType) {
-    skip = 2;
-    span.innerText = span.innerText + arr[wordIdx][currentIndex];
-    currentIndex++;
-  } else {
-    span.innerText = span.innerText.slice(0, span.innerText.length - 1);
-    currentIndex--;
-  }
+function autoType(arr,element){
+   const intervalId = setInterval(() => {
+    if (skip) {
+      skip--;
+      return;
+    }
+    if (!reverseType) {
+      skip = 2;
+      element.innerText = element.innerText + arr[wordIdx][currentIndex];
+      currentIndex++;
+    } else {
+      element.innerText = element.innerText.slice(0, element.innerText.length - 1);
+      currentIndex--;
+    }
+  
+    if (currentIndex == arr[wordIdx].length) {
+      skip = 5;
+      reverseType = true;
+    }
+  
+    if (element.innerText.length === 0 && reverseType) {
+      reverseType = false;
+      wordIdx++;
+    }
+  
+    if (wordIdx === arr.length) {
+      wordIdx = 0;
+    }
+  }, 100);
+  
+}
 
-  if (currentIndex == arr[wordIdx].length) {
-    skip = 5;
-    reverseType = true;
-  }
+autoType(list1,span2)
 
-  if (span.innerText.length === 0 && reverseType) {
-    reverseType = false;
-    wordIdx++;
-  }
-
-  if (wordIdx === arr.length) {
-    wordIdx = 0;
-  }
-}, 100);
+// autoType(list2,span)

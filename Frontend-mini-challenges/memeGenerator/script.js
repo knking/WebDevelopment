70d8dd4c6cab6719bbc62c1020fe210ecaad740e
knking <knr020498@gmail.com>
2024-12-29 18:11:05 +0530

    meme generator

--- Frontend-mini-challenges/memeGenerator/script.js
+++ Frontend-mini-challenges/memeGenerator/script.js
@@ -1,23 +1,41 @@
 
 const btn =  document.querySelector(".generate")
 
-const title =  document.querySelector(".meme-title")
-const author =  document.querySelector(".author span")
-const image =  document.querySelector("img")
+const memetitle =  document.querySelector(".meme-title")
+const memeauthor =  document.querySelector(".author span")
+const memeimage =  document.querySelector("img")
 
 
 // const url = "https://meme-api.com/gimme/wholesomememes";
 
 btn.addEventListener('click',()=>{
+    // fetch("https://meme-api.com/gimme/wholesomememes")
+    // .then((res)=> res.json())
+    // .then((data)=>{
+    //     const {author, title, url} = data
+    //     memetitle.innerText =  title
+    //     memeauthor.innerText = author
+    //     memeimage.src=`${url}`
+    // })
+    // .catch((e)=>{
+    //     console.log("Error: ", e);
+        
+    // })
+    getMeme()
+})
+
+function getMeme(){
     fetch("https://meme-api.com/gimme/wholesomememes")
     .then((res)=> res.json())
     .then((data)=>{
-        title.innerText =  data.title
-        author.innerText = data.author
-        image.src=`${data.url}`
+        const {author, title, url} = data
+        memetitle.innerText =  title
+        memeauthor.innerText = author
+        memeimage.src=`${url}`
     })
     .catch((e)=>{
         console.log("Error: ", e);
         
     })
-})
+}
+getMeme()

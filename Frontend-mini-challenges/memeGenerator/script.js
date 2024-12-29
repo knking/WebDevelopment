74dd953ee5c15400335ee387f1d99cf3b6406666
knking <knr020498@gmail.com>
2024-12-29 11:51:28 +0530

    meme generator

--- Frontend-mini-challenges/memeGenerator/script.js
+++ Frontend-mini-challenges/memeGenerator/script.js
@@ -1,31 +1,23 @@
 
-const imgUrl =  document.querySelector(".image img")
-const span =  document.querySelector(".image-sec p span")
-const h3 =  document.querySelector(".image-sec h3")
+const btn =  document.querySelector(".generate")
+
+const title =  document.querySelector(".meme-title")
+const author =  document.querySelector(".author span")
+const image =  document.querySelector("img")
+
 
 // const url = "https://meme-api.com/gimme/wholesomememes";
 
-const res = fetch("https://meme-api.com/gimme/wholesomememes");
-res
-  .then((data) => data.json())
-  .then((d) => {
-    console.log(d);
-    console.log(d.url);
-    console.log(d.title);
-    h3.innerText = d.title
-    console.log(d.author);
-    imgUrl.src=`${d.url}`
-    span.innerText = d.author
-  })
-  .catch((e) => {
-    console.log("error", e);
-  });
-
-// async function getData() {
-//     const response = await fetch("https://meme-api.com/gimme/wholesomememes")
-//     const data = await response.json()
-//     console.log(data);
-    
-    
-// }
-// getData()
+btn.addEventListener('click',()=>{
+    fetch("https://meme-api.com/gimme/wholesomememes")
+    .then((res)=> res.json())
+    .then((data)=>{
+        title.innerText =  data.title
+        author.innerText = data.author
+        image.src=`${data.url}`
+    })
+    .catch((e)=>{
+        console.log("Error: ", e);
+        
+    })
+})

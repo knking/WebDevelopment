5c013b8190afa83af4e8416355a7727dbcb14f34
knking <knr020498@gmail.com>
2024-12-29 10:21:54 +0530

    meme generator

--- Frontend-mini-challenges/memeGenerator/script.js
+++ Frontend-mini-challenges/memeGenerator/script.js
@@ -1,23 +1,31 @@
-const url = "https://meme-api.com/gimme/wholesomememes";
+
+const imgUrl =  document.querySelector(".image img")
+const span =  document.querySelector(".image-sec p span")
+const h3 =  document.querySelector(".image-sec h3")
+
+// const url = "https://meme-api.com/gimme/wholesomememes";
 
 const res = fetch("https://meme-api.com/gimme/wholesomememes");
-// res
-//   .then((data) => data.json())
-//   .then((d) => {
-//     console.log(d);
-//     console.log(d.url);
-//     console.log(d.title);
-//     console.log(d.author);
-//   })
-//   .catch((e) => {
-//     console.log("error", e);
-//   });
+res
+  .then((data) => data.json())
+  .then((d) => {
+    console.log(d);
+    console.log(d.url);
+    console.log(d.title);
+    h3.innerText = d.title
+    console.log(d.author);
+    imgUrl.src=`${d.url}`
+    span.innerText = d.author
+  })
+  .catch((e) => {
+    console.log("error", e);
+  });
 
-async function getData() {
-    const response = await fetch("https://meme-api.com/gimme/wholesomememes")
-    const data = await response.json()
-    console.log(data);
+// async function getData() {
+//     const response = await fetch("https://meme-api.com/gimme/wholesomememes")
+//     const data = await response.json()
+//     console.log(data);
     
     
-}
-getData()
+// }
+// getData()

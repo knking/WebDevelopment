17e638730a37ad6ceb58704d1491265ac55d2423
knking <knr020498@gmail.com>
2024-12-30 13:44:16 +0530

    Toast popup

--- Frontend-mini-challenges/ToastPopup/script.js
+++ Frontend-mini-challenges/ToastPopup/script.js
@@ -1,7 +1,38 @@
+const horizontalPosition = document.querySelector("#horizontal-position");
+const showTost = document.querySelector(".show");
+const tost = document.querySelector(".tost");
+const verticalposition = document.querySelector("#vertical-position");
+const tostContainer = document.querySelector(".tost-container");
+const tostType = document.querySelector("#tost-type");
+const tostMsg = document.querySelector(".tost-msg");
 
-const horizontalPosition = document.querySelector("#horizontal-position")
-const showText = document.querySelector(".show")
-const tost = document.querySelector(".tost")
-const verticalposition =  document.querySelector("#vertical-position")
+showTost.addEventListener("click", () => {
 
 
+  const newToast = document.createElement("div");
+  
+  newToast.classList.add("tost");
+  newToast.classList.add(tostType.value)
+  console.log(tostType.value);
+  
+  newToast.innerText = `${tostMsg.value}`;
+
+  const closeicon = document.createElement("span")
+  closeicon.innerText = ' ✗'
+    newToast.append(closeicon)
+
+  tostContainer.append(newToast);
+
+
+  if (horizontalPosition.value === "right") {
+    tostContainer.classList.add("right");
+  } else {
+    tostContainer.classList.remove("right");
+  }
+  if (verticalposition.value === "bottom") {
+    tostContainer.classList.add("bottom");
+   
+  } else {
+    tostContainer.classList.remove("bottom");
+  }
+});

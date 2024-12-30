398f64656e98143d0d888c5bdd671bd216614c98
knking <knr020498@gmail.com>
2024-12-30 08:57:59 +0530

    Toast popup

---
+++ Frontend-mini-challenges/ToastPopup/script.js
@@ -0,0 +1,7 @@
+
+const horizontalPosition = document.querySelector("#horizontal-position")
+const showText = document.querySelector(".show")
+const tost = document.querySelector(".tost")
+const verticalposition =  document.querySelector("#vertical-position")
+
+

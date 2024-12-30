cf343995e9f61b0db07c6e1b40442cd124a2dd6e
knking <knr020498@gmail.com>
2024-12-30 18:50:13 +0530

    Toast popup completed

--- Frontend-mini-challenges/ToastPopup/script.js
+++ Frontend-mini-challenges/ToastPopup/script.js
@@ -5,34 +5,55 @@ const verticalposition = document.querySelector("#vertical-position");
 const tostContainer = document.querySelector(".tost-container");
 const tostType = document.querySelector("#tost-type");
 const tostMsg = document.querySelector(".tost-msg");
+const duration = document.querySelector("#duration")
 
 showTost.addEventListener("click", () => {
 
 
+
+    if (horizontalPosition.value === "right") {
+        tostContainer.classList.add("right");
+      } else {
+        tostContainer.classList.remove("right");
+      }
+      if (verticalposition.value === "bottom") {
+        tostContainer.classList.add("bottom");
+      } else {
+        tostContainer.classList.remove("bottom");
+      }
+
   const newToast = document.createElement("div");
-  
+
   newToast.classList.add("tost");
-  newToast.classList.add(tostType.value)
+  newToast.classList.add(tostType.value);
   console.log(tostType.value);
-  
+
   newToast.innerText = `${tostMsg.value}`;
 
-  const closeicon = document.createElement("span")
-  closeicon.innerText = ' ✗'
-    newToast.append(closeicon)
+  const closeicon = document.createElement("span");
+  closeicon.innerText = " ✗";
+  newToast.append(closeicon);
+
+  function removeTost() {
+    if(tostContainer.classList.contains('right')){
+        newToast.classList.add('goRight')
+    }
+    else{
+        newToast.classList.add("goLeft");
+    }
+  
+    setTimeout(() => {
+      newToast.remove();
+    }, 100);
+  }
+  closeicon.addEventListener("click", () => {
+    removeTost();
+  });
+
+  setTimeout(()=>{
+    removeTost()
+  },parseInt(duration.value) * 1000)
 
   tostContainer.append(newToast);
 
-
-  if (horizontalPosition.value === "right") {
-    tostContainer.classList.add("right");
-  } else {
-    tostContainer.classList.remove("right");
-  }
-  if (verticalposition.value === "bottom") {
-    tostContainer.classList.add("bottom");
-   
-  } else {
-    tostContainer.classList.remove("bottom");
-  }
 });

d5a72a9d9753296dd7cd79ec375e1db668fd3cf1
knking <knr020498@gmail.com>
2025-01-03 13:19:36 +0530

    React day2

---
+++ React/01basicviteapp/demoapp/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+
+function Header(){
+  return(
+    <div>
+<p>i am first para</p>
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <>
+     <h1>I am First react app</h1>
+    </>
+  )
+}
+
+export  {App, Header}

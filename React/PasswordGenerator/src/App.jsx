c3251443d753bd86b643b65fc021e864b6b0e039
knking <knr020498@gmail.com>
2025-01-17 08:38:17 +0530

    password generator project completed

--- React/PasswordGenerator/src/App.jsx
+++ React/PasswordGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useState, useEffect } from "react";
+import { useCallback, useState, useEffect, useRef } from "react";
 import "./App.css";
 
 function App() {
@@ -6,6 +6,8 @@ function App() {
   const [lenght, setlenght] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
+
+  const passwordRef = useRef(null);
 
   const passwordGenerator = useCallback(() => {
     let str = "abcdefghijklmnopqrstuvwxyzABCDEFGHUJKLMNOPQRSTUVWXYZ";
@@ -17,24 +19,36 @@ function App() {
       let val = Math.floor(Math.random() * str.length + 0);
       pass += str[val];
     }
-    setPassword(pass)
+    setPassword(pass);
   }, [lenght, numberAllowed, charAllowed, setPassword]);
 
-useEffect(()=>{
- passwordGenerator()
-},[lenght,numberAllowed,charAllowed,passwordGenerator])
+  const copyToClipBoard = useCallback(() => {
+    passwordRef.current?.select()
+    // passwordRef.current?.setSelectionRange(0,10)
+    window.navigator.clipboard.writeText(password)
+  }, [password]);
+
+
+  useEffect(() => {
+    passwordGenerator();
+  }, [lenght, numberAllowed, charAllowed, passwordGenerator]);
 
   return (
     <>
-      <div className="w-[550px] h-28 bg-slate-500 rounded-xl mx-auto mt-32 p-2">
+      <div className="w-[550px] h-36 bg-slate-500 rounded-xl mx-auto mt-32 p-2 ">
+        <h1 className="text-white">Password Generator</h1>
         <input
           type="text"
-          className="mt-3 p-2 w-[475px] rounded-l"
+          className="mt-3 p-2 w-[475px] rounded-l outline-none"
           value={password}
           placeholder="Password"
           readOnly
+          ref={passwordRef}
         />
-        <button className="p-2 bg-blue-700 text-white rounded-r outline-none ">
+        <button
+          className="p-2 bg-blue-700 text-white rounded-r outline-none active:bg-blue-900"
+          onClick={copyToClipBoard}
+        >
           Copy
         </button>
         <div className="mt-4 text-orange-600 w-96 text-center flex gap-2">
@@ -50,14 +64,24 @@ useEffect(()=>{
           />
           <label htmlFor="length">Length({lenght})</label>
 
-          <input type="checkbox" name="number" defaultChecked={numberAllowed}  onChange={()=>{
-            setNumberAllowed((prev) => !prev)
-          }}/>
+          <input
+            type="checkbox"
+            name="number"
+            defaultChecked={numberAllowed}
+            onChange={() => {
+              setNumberAllowed((prev) => !prev);
+            }}
+          />
           <label htmlFor="number">Number</label>
 
-          <input type="checkbox" name="character" defaultChecked={charAllowed} onChange={()=>{
-            setCharAllowed((prev)=> !prev)
-          }} />
+          <input
+            type="checkbox"
+            name="character"
+            defaultChecked={charAllowed}
+            onChange={() => {
+              setCharAllowed((prev) => !prev);
+            }}
+          />
           <label htmlFor="character">Characters</label>
         </div>
       </div>

df4a910698911552b0db1c8a1c6421744cde277a
knking <knr020498@gmail.com>
2025-04-07 18:26:57 +0530

    practice project

--- React/Project/accordian/src/components/image-slider/ImageSlider.jsx
+++ React/Project/accordian/src/components/image-slider/ImageSlider.jsx
@@ -1,34 +1,51 @@
 import React, { useState } from "react";
 import myData from "./data";
 
-
-
 function ImageSlider() {
-    const [index, setIndex] = useState(0)
+  const [index, setIndex] = useState(0);
 
-    function handleNext(){
-        setIndex((prevIndex) => (prevIndex+1) % myData.length)
-        console.log("HI");
-        
-    }
+  function handleNext() {
+    // setIndex((prevIndex) => (prevIndex+1) % myData.length)
+    // console.log("HI");
+    setIndex(index === myData.length - 1 ? 0 : index + 1);
+  }
+
+  function handlePrevious() {
+    // setIndex((prevIndex) => (prevIndex+1) % myData.length)
+    // console.log("HI");
+    setIndex(index === 0 ? myData.length - 1 : index - 1);
+  }
 
   return (
-    <div className="w-full h-screen  flex justify-center items-center">
+    <div className="relative flex justify-center items-center w-[500px] h-[350px]">
+      <div className=" text-6xl absolute text-white left-4 cursor-pointer" onClick={handlePrevious}>
+        &larr;
+      </div>
+      
+        <img className="border-2 shadow-sm w-[100%] h-[100%]" src={myData[index].image} />
 
-        <div className="w-48 h-48 border border-amber-200">
-            <div className="flex justify-evenly items-start">
-            
-            <div className=" text-4xl"> &larr; </div>
+      <div className="text-6xl absolute text-white right-4 cursor-pointer " onClick={handleNext}>
+        &rarr;
+      </div>
 
-            <div className="text-4xl" onClick={handleNext}> &rarr; </div>
-            <div><img src={myData[index].image}/></div>
-            </div>
-            
+      <span className="flex absolute bottom-4">
+        {myData.map((_, index) => (
+          <button className="bg-white h-[15px] w-[15px] rounded-full border-none outline-none my-0 mx-1 cursor-pointer" key={index} onClick={()=>setIndex(index)}></button>
+        ))}
+      </span>
+
+
+      {/* <div className="w-[450px] h-[300px] border border-amber-200 relative">
+        <div><img className="w-full absolute" src={myData[index].image}/></div>
+        <div className="flex justify-between">
+        <div className=" text-4xl" onClick={handlePrevious}> &larr; </div>
+        <div className="text-4xl" onClick={handleNext}> &rarr; </div>
         </div>
-   
+            
+            </div>
+              */}
     </div>
   );
 }
 
 export default ImageSlider;
-

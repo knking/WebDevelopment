ec411986990affbef05ba39226620152dc862c41
knking <knr020498@gmail.com>
2025-04-08 11:14:33 +0530

    practice project

--- React/Project/accordian/src/components/image-slider/ImageSlider.jsx
+++ React/Project/accordian/src/components/image-slider/ImageSlider.jsx
@@ -2,18 +2,18 @@ import React, { useState } from "react";
 import myData from "./data";
 
 function ImageSlider() {
-  const [index, setIndex] = useState(0);
+  const [idx, setIdx] = useState(0);
 
   function handleNext() {
     // setIndex((prevIndex) => (prevIndex+1) % myData.length)
     // console.log("HI");
-    setIndex(index === myData.length - 1 ? 0 : index + 1);
+    setIdx(idx === myData.length - 1 ? 0 : idx + 1);
   }
 
   function handlePrevious() {
     // setIndex((prevIndex) => (prevIndex+1) % myData.length)
     // console.log("HI");
-    setIndex(index === 0 ? myData.length - 1 : index - 1);
+    setIdx(idx === 0 ? myData.length - 1 : idx - 1);
   }
 
   return (
@@ -22,7 +22,7 @@ function ImageSlider() {
         &larr;
       </div>
       
-        <img className="border-2 shadow-sm w-[100%] h-[100%]" src={myData[index].image} />
+        <img className="border-2 shadow-sm w-[100%] h-[100%]" src={myData[idx].image} />
 
       <div className="text-6xl absolute text-white right-4 cursor-pointer " onClick={handleNext}>
         &rarr;
@@ -30,7 +30,7 @@ function ImageSlider() {
 
       <span className="flex absolute bottom-4">
         {myData.map((_, index) => (
-          <button className="bg-white h-[15px] w-[15px] rounded-full border-none outline-none my-0 mx-1 cursor-pointer" key={index} onClick={()=>setIndex(index)}></button>
+          <button  className={`" h-[15px] w-[15px] rounded-full border-none outline-none my-0 mx-1 cursor-pointer btn" ${index=== idx ? 'bg-gray-500' : 'bg-white'}`} key={index} onClick={()=>setIdx(index)}></button>
         ))}
       </span>
 
